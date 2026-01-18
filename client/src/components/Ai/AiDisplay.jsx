587193fdcb2d976cdc836bda82ae7c45a6b8fc9b
aditya690-code <adityaofficial690@gmail.com>
2026-01-18 19:08:32 +0530

    Create AiDisplay component

--- client/src/components/Ai/AiDisplay.jsx
+++ client/src/components/Ai/AiDisplay.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const AiDisplay = () => {
   return (
-    <div>
+    <div className="display sticky bottom-0 w-full flex flex-col gap-2 bg-transparent flex-1 py-2 px-4 overflow-y-scroll no-scrollbar">
       
     </div>
-  )
-}
+  );
+};
 
-export default AiDisplay
+export default AiDisplay;
